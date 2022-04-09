5a92bdf4b28069d04255e6d4538f056d04773189
Dylan <Dylinger2002@gmail.com>
2022-04-09 14:47:33 -0400

    drawer menu layout finished~ logic still needed to switch between fragments~

--- DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
+++ DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
@@ -1,34 +1,50 @@
 package dylan.ashton.n01442206;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 //Dylan Ashton n01442206 RNA
-public class DylanActivity extends AppCompatActivity {
+public class DylanActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //FOR DESIGN
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dylan_main);
+
     }
 
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
     public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.DA2206);
         builder.setMessage(R.string.soa);
-        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
+        builder.setPositiveButton(R.string.yes, (dialog, id) -> {
 
-            }
         });
-        builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                DylanActivity.super.finish();
-            }
-        });
+        builder.setNegativeButton(R.string.no, (dialog, id) -> DylanActivity.super.finish());
         builder.show();
     }
 }
