be598eaad4b75d5c6ec037808dbd5a1d9568e07d
Dylan <Dylinger2002@gmail.com>
2022-04-08 20:38:55 -0400

    init~

---
+++ DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
@@ -0,0 +1,14 @@
+package dylan.ashton.n01442206;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DylanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
