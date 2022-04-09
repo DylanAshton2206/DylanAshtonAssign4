d921359cdd4a12d59b1394ad95e960a681dd7793
Dylan <Dylinger2002@gmail.com>
2022-04-09 13:00:03 -0400

    added comments to xml and java files~ intercept back button~

--- DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
+++ DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
@@ -1,14 +1,34 @@
 package dylan.ashton.n01442206;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+//Dylan Ashton n01442206 RNA
 public class DylanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.DA2206);
+        builder.setMessage(R.string.soa);
+        builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                DylanActivity.super.finish();
+            }
+        });
+        builder.show();
     }
 }
