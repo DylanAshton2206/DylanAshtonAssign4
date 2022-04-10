ab548509eaaa229269a678a392749f0c551ebc96
Dylan <Dylinger2002@gmail.com>
2022-04-10 17:35:10 -0400

    added readme and gitignore~

---
+++ DylanAssign44/build/generated/source/buildConfig/debug/dylan/ashton/n01442206/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package dylan.ashton.n01442206;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "dylan.ashton.n01442206";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+DylanAshton n01442206
+https://github.com/DylanAshton2206/DylanAshtonAssign4
+# DylanAshtonAssign4
+App for lab 4.
+
