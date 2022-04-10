040ba97afdd6a35112b56240a6b93db3bfa84940
Dylan <Dylinger2002@gmail.com>
2022-04-10 15:28:57 -0400

    overflow menu~ giving up on three lines must slide to open drawer~

--- DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
+++ DylanAssign44/src/main/java/dylan/ashton/n01442206/DylanActivity.java
@@ -6,8 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -21,19 +27,15 @@ public class DylanActivity extends AppCompatActivity implements NavigationView.O
     private final SettingsFrag settings = new SettingsFrag();
     private final ShapeFrag shape = new ShapeFrag();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dylan_main);
-
         //setup
         getSupportFragmentManager().beginTransaction().replace(R.id.dylan_frame_layout, home).commit();
         NavigationView navigationView = findViewById(R.id.dylan_nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
-
 
     //menu logic
     @Override
@@ -74,6 +76,36 @@ public class DylanActivity extends AppCompatActivity implements NavigationView.O
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Get menu inflater object.
+        MenuInflater menuInflater = getMenuInflater();
+        // Inflate the custom overflow menu
+        menuInflater.inflate(R.menu.overflow, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Get clicked menu item id.
+        int itemId = item.getItemId();
+        if(itemId == R.id.dylan_help)
+        {
+            GoToWebPage("https://www.youtube.com/");
+        }else if(itemId == R.id.dylan_settings)
+        {
+            startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+        }
+        return true;
+    }
+
+    public void GoToWebPage(String yourUrl) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(yourUrl));
+        startActivity(intent);
+    }
+
+//handle app close
+    @Override
     public void onBackPressed() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(R.string.DA2206);
@@ -84,4 +116,5 @@ public class DylanActivity extends AppCompatActivity implements NavigationView.O
         builder.setNegativeButton(R.string.no, (dialog, id) -> DylanActivity.super.finish());
         builder.show();
     }
+
 }
